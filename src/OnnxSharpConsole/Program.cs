6b0c81e6e83494a5a0a436503361c38933965591
ntr <ntr@ihfood.dk>
2021-01-23 17:28:22 +0100

    Add console with example parsing

---
+++ src/OnnxSharpConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace OnnxSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Examples see https://github.com/onnx/models
+            var onnxFileName = @"TEST.onnx";
+            using (var input = File.OpenRead(onnxFileName))
+            {
+                var model = Onnx.ModelProto.Parser.ParseFrom(input);
+            }
+        }
+    }
+}
