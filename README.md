f29f60adab7420e53220f31e735e5125ecc53d24
ntr <ntr@ihfood.dk>
2021-01-24 12:33:37 +0100

    edits

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
-![Build and test](https://github.com/nietras/OnnxSharp/workflows/Build%20and%20test/badge.svg)
+![Build and test](https://github.com/nietras/OnnxSharp/workflows/.NET/badge.svg)
 [![NuGet](https://img.shields.io/nuget/v/OnnxSharp)](https://www.nuget.org/packages/OnnxSharp/)
 [![Downloads](https://img.shields.io/nuget/dt/OnnxSharp)](https://www.nuget.org/packages/OnnxSharp/)
 [![Stars](https://img.shields.io/github/stars/nietras/OnnxSharp)](https://github.com/nietras/OnnxSharp/stargazers)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE.md)
 
 # OnnxSharp
-ONNX format manipulation in C#
+ONNX format parsing and manipulation in C#.
 
 # Status
 Pretty much just:
 ```
 .\protoc.exe .\onnx.proto3 --csharp_out=OnnxSharp
+```
+
+# Example Code
+```csharp
+using System.IO;
+using System.Linq;
+using Google.Protobuf;
+
+// Examples see https://github.com/onnx/models
+var onnxInputFilePath = @"mnist-8.onnx";
+
+using var file = File.OpenRead(onnxInputFilePath);
+
+var model = Onnx.ModelProto.Parser.ParseFrom(file);
+
+var graph = model.Graph;
+var inputs = graph.Input;
+var values = graph.ValueInfo;
+var outputs = graph.Output;
 ```
--- src/OnnxSharpConsole/Program.cs
+++ src/OnnxSharpConsole/Program.cs
@@ -2,54 +2,45 @@
 using System.Linq;
 using Google.Protobuf;
 
-namespace OnnxSharpConsole
+// Examples see https://github.com/onnx/models
+var onnxInputFilePath = @"mnist-8.onnx";
+
+var onnxInputFileName = Path.GetFileNameWithoutExtension(onnxInputFilePath);
+var outputDirectory = Path.GetDirectoryName(onnxInputFilePath);
+
+using (var file = File.OpenRead(onnxInputFilePath))
 {
-    class Program
+    var model = Onnx.ModelProto.Parser.ParseFrom(file);
+    var graph = model.Graph;
+    // Shapes are defined in inputs, values and outputs
+    var inputs = graph.Input;
+    var values = graph.ValueInfo;
+    var outputs = graph.Output;
+
+    foreach (var value in inputs.Concat(values).Concat(outputs))
     {
-        static void Main(string[] args)
-        {
-            // Examples see https://github.com/onnx/models
-            var onnxInputFilePath = @"mnist-8.onnx";
+        var shape = value.Type.TensorType.Shape;
+        var dims = shape.Dim;
+        var dim = dims[0];
+        //dim.DimValue = -1;
+        dim.ClearValue();
+        dim.DimValue = -1; // Or don't set it
+        //dim.DimParam = "None"; // Or don't set it, unset dimension means dynamic
+    }
 
-            var onnxInputFileName = Path.GetFileNameWithoutExtension(onnxInputFilePath);
-            var outputDirectory = Path.GetDirectoryName(onnxInputFilePath);
+    var fileNameSuffix = "-dynamic-leading-dimension";
+    var outputFilePathPrefix = Path.Combine(outputDirectory, onnxInputFileName + fileNameSuffix);
 
-            using (var file = File.OpenRead(onnxInputFilePath))
-            {
-                var model = Onnx.ModelProto.Parser.ParseFrom(file);
-                var graph = model.Graph;
-                // Shapes are defined in inputs, values and outputs
-                var inputs = graph.Input;
-                var values = graph.ValueInfo;
-                var outputs = graph.Output;
+    var onnxOutputFilePath = outputFilePathPrefix + ".onnx";
+    using (var outputFile = File.Create(onnxOutputFilePath))
+    {
+        model.WriteTo(outputFile);
+    }
 
-                foreach (var value in inputs.Concat(values).Concat(outputs))
-                {
-                    var shape = value.Type.TensorType.Shape;
-                    var dims = shape.Dim;
-                    var dim = dims[0];
-                    //dim.DimValue = -1;
-                    dim.ClearValue();
-                    dim.DimValue = -1; // Or don't set it
-                    //dim.DimParam = "None"; // Or don't set it, unset dimension means dynamic
-                }
-
-                var fileNameSuffix = "-dynamic-leading-dimension";
-                var outputFilePathPrefix = Path.Combine(outputDirectory, onnxInputFileName + fileNameSuffix);
-
-                var onnxOutputFilePath = outputFilePathPrefix + ".onnx";
-                using (var outputFile = File.Create(onnxOutputFilePath))
-                {
-                    model.WriteTo(outputFile);
-                }
-
-                var jsonOnnxOutputFilePath = outputFilePathPrefix + ".json";
-                using (var output = new StreamWriter(jsonOnnxOutputFilePath))
-                {
-                    var fmt = new JsonFormatter(JsonFormatter.Settings.Default);
-                    fmt.Format(model, output);
-                }
-            }
-        }
+    var jsonOnnxOutputFilePath = outputFilePathPrefix + ".json";
+    using (var output = new StreamWriter(jsonOnnxOutputFilePath))
+    {
+        var fmt = new JsonFormatter(JsonFormatter.Settings.Default);
+        fmt.Format(model, output);
     }
 }
