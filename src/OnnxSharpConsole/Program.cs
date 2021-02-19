1a092ab06aa318d84542748575ba0552709bf6e7
ntr <ntr@ihfood.dk>
2021-02-19 14:30:43 +0100

    refactor out, some things don't work on mnist file

--- src/OnnxSharpConsole/Program.cs
+++ src/OnnxSharpConsole/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Google.Protobuf;
 using Onnx;
+using OnnxSharpConsole;
 
 Action<string> log = t => { Console.WriteLine(t); Trace.WriteLine(t); };
 
@@ -16,8 +17,8 @@ Action<string> log = t => { Console.WriteLine(t); Trace.WriteLine(t); };
 //modelHACK.WriteToFile(onnxOutputFilePathHACK);
 
 // Examples see https://github.com/onnx/models
-//var onnxInputFilePath = @"mnist-8.onnx";
-var onnxInputFilePath = @"C:\git\eis\ei4-analysis-models\build\OnnxRuntimeProfiler_AnyCPU_Debug\TR-CntkModel-SV-7.18.0.onnx";
+var onnxInputFilePath = @"mnist-8.onnx";
+//var onnxInputFilePath = @"C:\git\eis\ei4-analysis-models\build\OnnxRuntimeProfiler_AnyCPU_Debug\TR-CntkModel-SV-7.18.0.onnx";
 
 var onnxInputFileName = Path.GetFileNameWithoutExtension(onnxInputFilePath);
 var outputDirectory = Path.GetDirectoryName(onnxInputFilePath);
@@ -25,94 +26,17 @@ var outputDirectory = Path.GetDirectoryName(onnxInputFilePath);
 var model = ModelProto.Parser.ParseFromFile(onnxInputFilePath);
 log($"Parsed file '{onnxInputFilePath}' of size {model.CalculateSize()}");
 
-model.RemoveInitializersFromInputs();
+// Save unmodified
+var unmodifiedjsonOnnxOutputFilePath = Path.Combine(outputDirectory, onnxInputFileName) + ".json";
+model.WriteJsonToFile(unmodifiedjsonOnnxOutputFilePath);
+
+//model.RemoveInitializersFromInputs();
 model.RemoveUnnecessaryInitializerReshapes();
 
 var graph = model.Graph;
-// Shapes are defined in inputs, values and outputs
-var inputs = graph.Input;
-var values = graph.ValueInfo;
-var nodes = graph.Node;
-var initializers = graph.Initializer;
-var outputs = graph.Output;
 
-var allValues = inputs.Concat(values).Concat(outputs).ToArray();
+//Brainstorm.SetDim(graph);
 
-//var namesToIgnore = new HashSet<string>();
-var reshapes = nodes.Where(n => n.OpType == "Reshape").ToArray();
-foreach (var reshape in reshapes)
-{
-    // Reshape has two inputs (data and shape)
-    // Second hence is the shape
-    var shapeInputName = reshape.Input[1];
-    var shape = initializers.Single(v => v.Name == shapeInputName);
-    Debug.Assert(shape.DataType == (int)TensorProto.Types.DataType.Int64);
-    var dims = shape.Dims;
-    if (dims.Count > 0 && dims[0] > 0)
-    {
-        // Data may be stored as Int64 or Raw (fixed-width, little-endian)
-        if (shape.Int64Data.Count > 0)
-        {
-            var int64Data = shape.Int64Data;
-            if (int64Data[0] == 1) // Dimension we replace
-            {
-                int64Data[0] = -1;
-            }
-        }
-        if (!shape.RawData.IsEmpty)
-        {
-            var rawData = shape.RawData;
-            var rawAsInt64Data = MemoryMarshal.Cast<byte, long>(rawData.Span);
-            Debug.Assert(rawAsInt64Data.Length == dims[0]);
-            if (rawAsInt64Data[0] == 1) // Dimension we replace
-            {
-                var newShape = rawAsInt64Data.ToArray();
-                newShape[0] = -1;
-                shape.RawData = ByteString.CopyFrom(MemoryMarshal.Cast<long, byte>(newShape.AsSpan()));
-            }
-        }
-        else
-        {
-            // FAILS IF ADDING
-            //foreach (var output in reshape.Output)
-            //{
-            //    namesToIgnore.Add(output);
-            //}
-        }
-    }
-    // 
-    log(reshape.ToString());
-}
-
-foreach (var value in allValues)
-{
-    //var denotation = value.Type.Denotation;
-    //if (!namesToIgnore.Contains(value.Name))
-    {
-        var shape = value.Type.TensorType.Shape;
-        var dims = shape.Dim;
-        var dim = dims[0];
-        dim.ClearValue();
-        //dim.DimValue = -1; // Or don't set it
-        dim.DimParam = "N";
-    }
-    //else
-    //{
-    //    // FAILS IF WE DO NOT CHANGE OUTPUT OF THE ONE RESHAPE
-    //    // Reshape
-    //    log(value.ToString());
-    //}
-}
-
-//foreach (var node in nodes)
-//{
-//    var shape = node.Output;
-//    var dims = shape.Dim;
-//    var dim = dims[0];
-//    dim.ClearValue();
-//    //dim.DimValue = -1; // Or don't set it
-//    dim.DimParam = "N";
-//}
 
 var fileNameSuffix = "-dynamic-leading-dimension";
 var outputFilePathPrefix = Path.Combine(outputDirectory, onnxInputFileName + fileNameSuffix);
