72ffd6bc4441b3c2e28d0c559a602173a72bab7e
ntr <ntr@ihfood.dk>
2021-02-27 16:33:55 +0100

    edit

--- src/OnnxSharp/TensorProtoExtensions.cs
+++ src/OnnxSharp/TensorProtoExtensions.cs
@@ -7,10 +7,7 @@ namespace Onnx
         public static TensorProto.Types.DataType DataType(this TensorProto tensor) =>
             (TensorProto.Types.DataType)tensor.DataType;
 
-        public static TensorSummary Summary(this TensorProto tensor)
-        {
-
-            return new TensorSummary(tensor.Name, tensor.DataType(), tensor.Dims.ToArray(), tensor.CalculateSize());
-        }
+        public static TensorSummary Summary(this TensorProto tensor) =>
+            new TensorSummary(tensor.Name, tensor.DataType(), tensor.Dims.ToArray(), tensor.CalculateSize());
     }
 }
